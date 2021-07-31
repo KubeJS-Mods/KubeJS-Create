908b41799fa9a944591eddf98cf33ec7f8703b9c
Max <maxh2709@gmail.com>
2021-07-31 14:53:39 +0200

    Add sequenced assembly item type

--- src/main/java/dev/latvian/kubejs/create/KubeJSCreatePlugin.java
+++ src/main/java/dev/latvian/kubejs/create/KubeJSCreatePlugin.java
@@ -1,15 +1,30 @@
 package dev.latvian.kubejs.create;
 
 import com.simibubi.create.AllRecipeTypes;
+import com.simibubi.create.content.contraptions.itemAssembly.SequencedAssemblyItem;
 import com.simibubi.create.content.contraptions.processing.ProcessingRecipeSerializer;
 import dev.latvian.kubejs.KubeJSPlugin;
+import dev.latvian.kubejs.item.ItemBuilder;
+import dev.latvian.kubejs.item.custom.ItemType;
+import dev.latvian.kubejs.item.custom.ItemTypes;
 import dev.latvian.kubejs.recipe.RegisterRecipeHandlersEvent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 
 /**
  * @author LatvianModder
  */
 public class KubeJSCreatePlugin extends KubeJSPlugin {
+	@Override
+	public void init() {
+		ItemTypes.register(new ItemType("create:sequenced_assembly") {
+			@Override
+			public Item createItem(ItemBuilder itemBuilder) {
+				return new SequencedAssemblyItem(itemBuilder.createItemProperties());
+			}
+		});
+	}
+
 	@Override
 	public void addRecipes(RegisterRecipeHandlersEvent event) {
 		event.register(new ResourceLocation("create:sequenced_assembly"), SequencedAssemblyRecipeJS::new);
