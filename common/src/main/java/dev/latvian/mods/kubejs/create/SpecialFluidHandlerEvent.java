561f32bc122ae56971d648f6d60e09a9c8ceb100
AK9™ <anthonkrisat@hotmail.com>
2022-05-25 02:09:51 +0200

    Move to Architectury, add Create Fabric support
    (Squashed version of #8 and a subsequent fix commit)

--- common/src/main/java/dev/latvian/mods/kubejs/create/SpecialFluidHandlerEvent.java
+++ common/src/main/java/dev/latvian/mods/kubejs/create/SpecialFluidHandlerEvent.java
@@ -2,11 +2,10 @@ package dev.latvian.mods.kubejs.create;
 
 import com.simibubi.create.content.contraptions.fluids.OpenEndedPipe;
 import com.simibubi.create.foundation.fluid.FluidIngredient;
-import dev.architectury.hooks.fluid.forge.FluidStackHooksForge;
+import dev.latvian.mods.kubejs.create.platform.FluidIngredientHelper;
 import dev.latvian.mods.kubejs.event.EventJS;
 import dev.latvian.mods.kubejs.fluid.FluidStackJS;
 import dev.latvian.mods.kubejs.util.MapJS;
-import net.minecraftforge.fluids.FluidStack;
 
 import java.util.List;
 import java.util.function.BiConsumer;
@@ -26,23 +25,13 @@ public class SpecialFluidHandlerEvent extends EventJS {
 	public void addFluidHandler(Object fluidStack, BiConsumer<OpenEndedPipe, FluidStackJS> handler) {
 		FluidIngredient fluidIngredient;
 		if (fluidStack instanceof FluidStackJS fluidStackJS) {
-			fluidIngredient = FluidIngredient.fromFluidStack(FluidStackHooksForge.toForge(fluidStackJS.getFluidStack()));
+			fluidIngredient = FluidIngredientHelper.toFluidIngredient(fluidStackJS);
 		} else if (fluidStack instanceof MapJS map && (map.containsKey("fluid") || map.containsKey("fluidTag"))) {
 			fluidIngredient = FluidIngredient.deserialize(map.toJson());
 		} else {
-			fluidIngredient = FluidIngredient.fromFluidStack(FluidStackHooksForge.toForge(FluidStackJS.of(fluidStack).getFluidStack()));
+			fluidIngredient = FluidIngredientHelper.toFluidIngredient(FluidStackJS.of(fluidStack));
 		}
 
-		fluidHandlers.add(new OpenEndedPipe.IEffectHandler() {
-			@Override
-			public boolean canApplyEffects(OpenEndedPipe pipe, FluidStack fluid) {
-				return fluidIngredient.test(fluid);
-			}
-
-			@Override
-			public void applyEffects(OpenEndedPipe pipe, FluidStack fluid) {
-				handler.accept(pipe, FluidStackJS.of(fluid));
-			}
-		});
+		fluidHandlers.add(FluidIngredientHelper.createEffectHandler(fluidIngredient, handler));
 	}
 }
