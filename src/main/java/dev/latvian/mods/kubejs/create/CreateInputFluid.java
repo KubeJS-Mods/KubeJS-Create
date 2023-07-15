d2c8ba60fd85ae483d89e2ed2cb48c15c4fa8b6d
Max <maxh2709@gmail.com>
2023-07-15 19:06:22 +0200

    Fix CreateInputFluid

--- src/main/java/dev/latvian/mods/kubejs/create/CreateInputFluid.java
+++ src/main/java/dev/latvian/mods/kubejs/create/CreateInputFluid.java
@@ -4,29 +4,29 @@ import com.simibubi.create.foundation.fluid.FluidIngredient;
 import dev.latvian.mods.kubejs.fluid.FluidLike;
 import dev.latvian.mods.kubejs.fluid.FluidStackJS;
 import dev.latvian.mods.kubejs.fluid.InputFluid;
+import dev.latvian.mods.kubejs.util.Tags;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.FluidTags;
 
 public record CreateInputFluid(FluidIngredient ingredient) implements InputFluid {
 	public static final CreateInputFluid EMPTY = new CreateInputFluid(FluidIngredient.EMPTY);
 
 	@Override
-	public boolean isEmpty() {
+	public boolean kjs$isEmpty() {
 		return ingredient.equals(FluidIngredient.EMPTY) || ingredient.getMatchingFluidStacks().isEmpty();
 	}
 
 	@Override
-	public long getAmount() {
+	public long kjs$getAmount() {
 		return ingredient.getRequiredAmount();
 	}
 
 	@Override
-	public FluidLike copy(long amount) {
+	public FluidLike kjs$copy(long amount) {
 		if (ingredient instanceof FluidIngredient.FluidStackIngredient in) {
 			var fs = in.getMatchingFluidStacks().get(0);
 			return FluidStackJS.of(fs.getFluid(), amount, fs.getTag() == null ? null : fs.getTag().copy());
 		} else if (ingredient instanceof FluidIngredient.FluidTagIngredient in && in.getRequiredAmount() != amount) {
-			var tag = FluidTags.create(new ResourceLocation(in.serialize().get("fluidTag").getAsString()));
+			var tag = Tags.fluid(new ResourceLocation(in.serialize().get("fluidTag").getAsString()));
 			return new CreateInputFluid(FluidIngredient.fromTag(tag, (int) amount));
 		}
 
