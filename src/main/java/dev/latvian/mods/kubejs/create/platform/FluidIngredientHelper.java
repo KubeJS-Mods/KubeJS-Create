9b8590d71dd3050f96efbc8201ee37ae4c38c468
max. <max@someone.ky>
2025-10-19 23:53:31 +0200

    Improve FluidIngredient type wrapping

--- src/main/java/dev/latvian/mods/kubejs/create/platform/FluidIngredientHelper.java
+++ src/main/java/dev/latvian/mods/kubejs/create/platform/FluidIngredientHelper.java
@@ -1,5 +1,6 @@
 package dev.latvian.mods.kubejs.create.platform;
 
+import com.mojang.serialization.DataResult;
 import com.simibubi.create.api.behaviour.spouting.BlockSpoutingBehaviour;
 import com.simibubi.create.api.effect.OpenPipeEffectHandler;
 import com.simibubi.create.api.registry.SimpleRegistry;
@@ -7,28 +8,64 @@ import com.simibubi.create.foundation.fluid.FluidIngredient;
 import dev.latvian.mods.kubejs.block.state.BlockStatePredicate;
 import dev.latvian.mods.kubejs.create.events.SpecialFluidHandlerEvent;
 import dev.latvian.mods.kubejs.create.events.SpecialSpoutHandlerEvent;
+import dev.latvian.mods.kubejs.error.KubeRuntimeException;
 import dev.latvian.mods.kubejs.fluid.FluidWrapper;
+import dev.latvian.mods.kubejs.script.SourceLine;
 import dev.latvian.mods.kubejs.util.RegistryAccessContainer;
 import dev.latvian.mods.rhino.Context;
+import net.minecraft.core.HolderSet;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.TagsUpdatedEvent;
 import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.FluidType;
+import net.neoforged.neoforge.fluids.crafting.DataComponentFluidIngredient;
 import net.neoforged.neoforge.fluids.crafting.EmptyFluidIngredient;
+import net.neoforged.neoforge.fluids.crafting.SingleFluidIngredient;
+import net.neoforged.neoforge.fluids.crafting.SizedFluidIngredient;
+import net.neoforged.neoforge.fluids.crafting.TagFluidIngredient;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import static com.mojang.serialization.DataResult.error;
+import static com.mojang.serialization.DataResult.success;
+
 public class FluidIngredientHelper {
 	public static FluidIngredient toFluidIngredient(FluidStack fluidStack) {
 		return FluidIngredient.fromFluidStack(fluidStack);
 	}
 
-	public static FluidIngredient convert(net.neoforged.neoforge.fluids.crafting.FluidIngredient fluidIngredient) {
-		return FluidIngredient.EMPTY;
+	public static FluidIngredient toFluidIngredient(TagKey<Fluid> tag) {
+		return FluidIngredient.fromTag(tag, FluidType.BUCKET_VOLUME);
+	}
+
+	public static FluidIngredient toFluidIngredient(Fluid fluid) {
+		return FluidIngredient.fromFluid(fluid, FluidType.BUCKET_VOLUME);
+	}
+
+	public static DataResult<FluidIngredient> convert(SizedFluidIngredient neoIn) {
+		return switch (neoIn.ingredient()) {
+			case SingleFluidIngredient single -> success(FluidIngredient.fromFluid(single.fluid().value(), neoIn.amount()));
+			case TagFluidIngredient tag -> success(FluidIngredient.fromTag(tag.tag(), neoIn.amount()));
+			case DataComponentFluidIngredient nbt -> switch (nbt.fluids()) {
+				case HolderSet.Direct<Fluid> __ when nbt.fluids().size() == 1 -> {
+					var fluid = nbt.fluids().get(0);
+					var stack = new FluidStack(fluid, neoIn.amount(), nbt.components().asPatch());
+
+					yield success(FluidIngredient.fromFluidStack(stack));
+				}
+				default -> error(() -> "Create FluidIngredients only support single fluids when using components!");
+
+			};
+			case EmptyFluidIngredient empty -> success(FluidIngredient.EMPTY);
+			default -> error(() -> "Create FluidIngredients only support single fluid, fluid with data, or tag ingredients!");
+		};
 	}
 
 	public static net.neoforged.neoforge.fluids.crafting.FluidIngredient convert(FluidIngredient fluidIngredient) {
@@ -97,12 +134,15 @@ public class FluidIngredientHelper {
 	}
 
 	public static FluidIngredient wrap(Context cx, Object o) {
-		if (o instanceof FluidStack fluidStackJS) {
-			return toFluidIngredient(fluidStackJS);
-		} else if (o instanceof Map<?, ?> map && map.containsKey("type")) {
-			return FluidIngredient.CODEC.parse(RegistryAccessContainer.of(cx).java(), map).getOrThrow();
-		} else {
-			return toFluidIngredient(FluidWrapper.wrap(RegistryAccessContainer.of(cx), o));
-		}
+		return (switch (o) {
+			case FluidIngredient id -> success(id);
+			case FluidStack stack -> success(toFluidIngredient(stack));
+			case TagKey<?> tag -> success(toFluidIngredient(FluidTags.create(tag.location())));
+			case Fluid fluid -> success(toFluidIngredient(fluid));
+			case SizedFluidIngredient neoIn -> convert(neoIn);
+			case net.neoforged.neoforge.fluids.crafting.FluidIngredient neoIn -> convert(new SizedFluidIngredient(neoIn, FluidType.BUCKET_VOLUME));
+			case Map<?, ?> map when map.containsKey("type") -> FluidIngredient.CODEC.parse(RegistryAccessContainer.of(cx).java(), map);
+			default -> convert(FluidWrapper.wrapSizedIngredient(cx, o));
+		}).getOrThrow(msg -> new KubeRuntimeException("Failed to parse Create FluidIngredient: " + msg).source(SourceLine.of(cx)));
 	}
 }
